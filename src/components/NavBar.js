e848abd5413ece32ecb0fc6a9046cb8271faecad
Luke <ldv432@gmail.com>
2024-04-30 13:29:50 -0600

    aint nobody got time for failed tests?

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,29 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
+    <nav
+            class="navbar"
+          >
+            <a
+              aria-current="page"
+              class="nav-link active"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              class="nav-link"
+              href="/directors"
+            >
+              Directors
+            </a>
+            <a
+              class="nav-link"
+              href="/actors"
+            >
+              Actors
+            </a>
+          </nav>
     );
 };
 
